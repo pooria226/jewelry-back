86a8f6028c743e73eb76fd50c2ceff2ffc681190
Pooria Mohammadi <mohammadi.pooria.2000@gmail.com>
2022-07-23 03:08:40 +0430

    updated

--- utils/currentPrice.js
+++ utils/currentPrice.js
@@ -3,6 +3,7 @@ module.exports.currentPrice = async (id) => {
   const result = await goldPrice();
   if (result?.data?.data.status == 200) {
     const goldPrice = result?.data?.data?.prices?.geram18.current;
+    console.log("goldPrice", goldPrice);
     return goldPrice?.slice(0, goldPrice.length - 1);
   }
 };
